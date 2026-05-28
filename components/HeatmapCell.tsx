2418c7395e94dca37adb4a4029262d72fa633eec
ykmshhh <yakukhno.maria@lll.kpi.ua>
2026-05-28 11:34:13 +0300

    feat: improve heatmap colors

--- components/HeatmapCell.tsx
+++ components/HeatmapCell.tsx
@@ -8,50 +8,52 @@ type HeatmapCellProps = {
 export default function HeatmapCell({value, opacity, mode, date,}: HeatmapCellProps) {
     let intensityClass = "";
 
+    const colors = {
+        commits: [
+            "bg-green-950",
+            "bg-green-800",
+            "bg-green-600",
+            "bg-green-500",
+            "bg-green-400",
+        ],
+
+        prs: [
+            "bg-blue-950",
+            "bg-blue-800",
+            "bg-blue-600",
+            "bg-blue-500",
+            "bg-blue-400",
+        ],
+
+        issues: [
+            "bg-amber-200",
+            "bg-amber-300",
+            "bg-amber-400",
+            "bg-amber-500",
+            "bg-amber-600",
+        ],
+    };
+
     if (value === 0) {
-        intensityClass = "opacity-0";
+        intensityClass = "bg-zinc-900";
     } else if (opacity < 0.2) {
-        intensityClass =
-            mode === "commits"
-                ? "bg-green-900"
-                : mode === "prs"
-                    ? "bg-blue-900"
-                    : "bg-amber-900";
+        intensityClass = colors[mode][0];
     } else if (opacity < 0.4) {
-        intensityClass =
-            mode === "commits"
-                ? "bg-green-700"
-                : mode === "prs"
-                    ? "bg-blue-700"
-                    : "bg-amber-700";
+        intensityClass = colors[mode][1];
     } else if (opacity < 0.6) {
-        intensityClass =
-            mode === "commits"
-                ? "bg-green-500"
-                : mode === "prs"
-                    ? "bg-blue-500"
-                    : "bg-amber-500";
+        intensityClass = colors[mode][2];
     } else if (opacity < 0.8) {
-        intensityClass =
-            mode === "commits"
-                ? "bg-green-400"
-                : mode === "prs"
-                    ? "bg-blue-400"
-                    : "bg-amber-400";
+        intensityClass = colors[mode][3];
     } else {
-        intensityClass =
-            mode === "commits"
-                ? "bg-green-300"
-                : mode === "prs"
-                    ? "bg-blue-300"
-                    : "bg-amber-300";
+        intensityClass = colors[mode][4];
     }
 
     return (
         <div
             title={`${date.toDateString()} - ${value} ${mode}`}
             className={`
-                w-[15px] h-[15px] rounded-sm
+                w-[15px] h-[15px]
+                rounded-sm
                 transition-all duration-200
                 cursor-pointer
                 ${intensityClass}
