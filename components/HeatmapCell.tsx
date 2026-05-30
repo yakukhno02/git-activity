b0d08d9b2a671bef838a25ec8346172f70452612
ykmshhh <yakukhno.maria@lll.kpi.ua>
2026-05-30 12:04:27 +0300

    feat: add custom heatmap tooltip

--- components/HeatmapCell.tsx
+++ components/HeatmapCell.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 type HeatmapCellProps = {
     value: number;
     opacity: number;
@@ -9,6 +10,7 @@ type HeatmapCellProps = {
 export default function HeatmapCell({value, opacity, color, date, showTooltip = true}: HeatmapCellProps) {
     let intensityClass = "";
 
+    const [hovered, setHovered] = useState(false);
     const colors = {
         green: [
             "bg-[#39d353]/20",
@@ -51,17 +53,48 @@ export default function HeatmapCell({value, opacity, color, date, showTooltip = 
 
     return (
         <div
-            title={showTooltip
-                ? `${date.toDateString()} - ${value}`
-                : undefined
-            }
-            className={`
+            className="relative inline-block"
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+        >
+            <div
+                className={`
                 w-[15px] h-[15px]
                 rounded-sm
                 transition-all duration-200
                 cursor-pointer
                 ${intensityClass}
             `}
-        />
+            />
+
+            {hovered && showTooltip && (
+                <div
+                    className="
+                    absolute
+                    bottom-6
+                    left-1/2
+                    -translate-x-1/2
+                    whitespace-nowrap
+                    rounded
+                    bg-zinc-800
+                    px-2
+                    py-1
+                    text-xs
+                    text-white
+                    shadow-lg
+                    z-50
+                    pointer-events-none
+                "
+                >
+                    {value}{" "}
+                    {color === "green"
+                        ? "commits"
+                        : color === "blue"
+                            ? "PRs"
+                            : "issues"}{" "}
+                    on {date.toLocaleDateString()}
+                </div>
+            )}
+        </div>
     );
 }
